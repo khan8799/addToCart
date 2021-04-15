20c758f57b0a8e66f0365e658b37bb5f267ad9eb
khan8799 <farmankhanmlc@gmail.com>
2021-04-15 11:08:09 +0530

    vs

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -3,6 +3,7 @@ import { CanActivate } from '@angular/router';
 import { AuthService } from './auth.service';
 import { Router, RouterStateSnapshot } from '@angular/router';
 import { map } from 'rxjs/operators';
+import { Observable, from, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,9 +13,9 @@ export class AuthGuardService implements CanActivate {
   constructor(private auth: AuthService, private router: Router) { }
 
   canActivate(route, state: RouterStateSnapshot) {
+
     return this.auth.user$.pipe(map(user => {
-      if (user)
-        return true;
+      if (user) return true;
 
       this.router.navigate(['/login'], { queryParams: { returnUrl: state.url}});
       return false;
